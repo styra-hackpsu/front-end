8b23fb5ed4508149eb335fe326b4c7695528a6f5
Rohan Dhar <rohan19443@iiitd.ac.in>
2021-03-20 06:17:10 +0530

    Add initial front-end code

---
+++ index.80fd1e57.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(()=>{alert("Testing build in parcel: 100")}));
+//# sourceMappingURL=index.80fd1e57.js.map
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import { z } from "./test";
+import "../style/index.css";
+
+window.addEventListener("DOMContentLoaded", () => {
+	alert(`Testing build in parcel: ${z}`);
+});
