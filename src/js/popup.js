4a87ecedfa378c4c78624c42416282cf225ab776
Rohan Jain <rohan19095@iiitd.ac.in>
2021-03-21 00:53:48 +0530

    get image base64

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,36 +1,37 @@
-var stream;
-// document.getElementById("but_start").addEventListener("click", async () => {
-// 	document.getElementById("text").style.display = "block";
-// 	await executeScript(getAccess);
-// 	await executeScript(setPageBackgroundColor);
-// });
+let imageCapture, photoWidth;
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded");
+  executeScript(getAccess);
+  // setInterval(() => executeScript(getAccess), 60000);
+});
 
 let executeScript = async (fun) => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: fun,
-	});
+  // const tab = await chrome.tabs.create({
+  //   active: false,
+  //   url: "https://www.google.com",
+  // });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: fun,
+  });
+  // chrome.tabs.remove(tab.id);
 };
 
-// will be executed in browser dom, need to send back stream from here.
 let getAccess = async () => {
-	navigator.mediaDevices
-		.getUserMedia({ video: true })
-		.then((stream_) => {
-			console.log(stream_);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(async (stream) => {
+      const track = stream.getVideoTracks()[0];
+      imageCapture = new ImageCapture(track);
+      var reader = new FileReader();
+      const blob = await imageCapture.takePhoto();
+      reader.readAsDataURL(blob);
+      reader.onloadend = function () {
+        var base64data = reader.result;
+        console.log(base64data);
+      };
+    })
+    .catch((error) => console.log(error));
 };
-
-let stopCamera = () => {};
-
-let sendStream = () => {};
-
-function setPageBackgroundColor() {
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color;
-	});
-}
