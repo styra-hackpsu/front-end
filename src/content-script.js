5fce4f80b467d3e77795b91d01b48684b3a33174
Rohan Dhar <rohan19443@iiitd.ac.in>
2021-03-21 23:58:22 +0530

    Add Number()

--- src/content-script.js
+++ src/content-script.js
@@ -1,59 +1,65 @@
 async function post(url, data) {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+	const response = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify(data),
+	});
 
-  const resData = await response.json();
-  return resData;
+	const resData = await response.json();
+	return resData;
 }
 
 function checkURL(url) {
-  if (url.indexOf("search_query") > -1) {
-    console.log("Youtube");
-    return false;
-  }
+	if (url.indexOf("search_query") > -1) {
+		console.log("Youtube");
+		return false;
+	}
 
-  if (url.indexOf("/search?q=") > -1) {
-    console.log("Google link");
-    return false;
-  }
+	if (url.indexOf("/search?q=") > -1) {
+		console.log("Google link");
+		return false;
+	}
 
-  return true;
+	return true;
 }
 
 function sendURL() {
-  const request = { url: window.location.href };
+	const request = { url: window.location.href };
 
-  if (checkURL(request.url)) {
-    post("http://127.0.0.1:8000/utils/change-detect/", { url: request.url })
-      .then((res) => {
-        if (res?.change_detected) {
-          chrome.storage.local.set({ pageMode: "distracted" });
-          chrome.storage.local.set({ pageData: JSON.stringify(res) });
-          setTimeout(
-            () => chrome.runtime.sendMessage({ popup_open_new_tab: true }),
-            2000
-          );
-        }
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }
+	if (checkURL(request.url)) {
+		post("http://127.0.0.1:8000/utils/change-detect/", { url: request.url })
+			.then((res) => {
+				if (res?.change_detected) {
+					chrome.storage.local.set({ pageMode: "distracted" });
+					chrome.storage.local.set({ pageData: JSON.stringify(res) });
+					setTimeout(
+						() =>
+							chrome.runtime.sendMessage({
+								popup_open_new_tab: true,
+							}),
+						2000
+					);
+				}
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	}
 }
 
 chrome.storage.local.get(
-  ["snoozeTill", "breakTill"],
-  ({ snoozeTill, breakTill }) => {
-    if ((!breakTill || breakTill === 0) && (!snoozeTill || snoozeTill === 0)) {
-      sendURL();
-    }
-  }
+	["snoozeTill", "breakTill"],
+	({ snoozeTill, breakTill }) => {
+		if (
+			(!breakTill || Number(breakTill) === 0) &&
+			(!snoozeTill || Number(snoozeTill) === 0)
+		) {
+			sendURL();
+		}
+	}
 );
 
 /* // TODO :
